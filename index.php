aa778ec18de79a400b0d310f173317c995467b3e
kriper0nind <kriper0nind@gmail.com>
2020-02-18 18:58:44 +0300

    update 2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 ﻿<?php
-
+		error_reporting(0);
     $sortId = 1;
     
     if(!empty($_GET['sort_id'])){
@@ -63,84 +63,65 @@
     
 </head>
 <body>
-
-		<header>
-			<div class="content">
-				<h1 class="name">домашний текстиль</h1>
-				<p class="nPhone">+7 (904) 214-82-18</p>
-			</div>
-			<div class="BG">
-				<img src="img/oneBG.jpg" width="160" height="180" class="one-bg">
-				<img src="img/twoBG.jpg" width="140" height="160" class="two-bg">
-				<img src="img/threeBG.jpg" width="90" height="110" class="three-bg">
-				<img src="img/fourBG.jpg" width="90" height="110" class="four-bg">
-				<img src="img/fiveBG.jpg" width="140" height="160" class="five-bg">
-				<img src="img/sixBG.jpg" width="160" height="130" class="six-bg">
-			</div>
-			<div class="BB">
-				<img src="img/oneBB.jpg" width="140" height="160" class="one-bb">
-				<img src="img/twoBB.jpg" width="120" height="140" class="two-bb">
-				<img src="img/threeBB.jpg" width="90" height="90" class="three-bb">
-				<img src="img/fourBB.jpg" width="90" height="110" class="four-bb">
-				<img src="img/fiveBB.jpg" width="150" height="100" class="five-bb">
-				<img src="img/sixBB.jpg" width="140" height="160" class="six-bb">
-			</div>
-		</header>
-		<div class="wrapper">
-			<h1 class="whatSelling">что мы продаем</h1>
-			<div class="state">
-				<button class="itemBtn-news btn" onclick="location.href='<?php echo "{$_SERVER['PHP_SELF']}?sort_id=1"; ?>'">новинки</button>
-				<button class="itemBtn-popular btn" onclick="location.href='<?php echo "{$_SERVER['PHP_SELF']}?sort_id=2"; ?>'">популярное</button>
-				<button class="itemBtn-premium btn" onclick="location.href='<?php echo "{$_SERVER['PHP_SELF']}?sort_id=3"; ?>'">по алфавиту</button>
-			</div>
-			<div class="cards" overflow-y:scroll>
-				<div class="card-item">
-					<h2 class="item-name">тест</h2>
-					<img src="img/fiveBG.jpg" width="300px" height="300px" class="item-img">
-					<a href="#"><button class="btn-item btn">подробнее</button></a>
-					<h3 class="price-item">900р</h3>
-				</div>
-				<div class="card-item">
-					<h2 class="item-name">второй тест</h2>
-					<img src="img/fiveBG.jpg" width="300px" height="300px" class="item-img">
-					<a href="#"><button class="btn-item btn">подробнее</button></a>
-					<h3 class="price-item">900р</h3>
-				</div>
-				<div class="card-item">
-					<h2 class="item-name">второй тест</h2>
-					<img src="img/fiveBG.jpg" width="300px" height="300px" class="item-img">
-					<a href="#"><button class="btn-item btn">подробнее</button></a>
-					<h3 class="price-item">900р</h3>
-				</div>
-				<div class="card-item">
-					<h2 class="item-name">второй тест</h2>
-					<img src="img/fiveBG.jpg" width="300px" height="300px" class="item-img">
-					<a href="#"><button class="btn-item btn">подробнее</button></a>
-					<h3 class="price-item">900р</h3>
-				</div>
-				<div class="card-item">
-					<h2 class="item-name">второй тест</h2>
-					<img src="img/fiveBG.jpg" width="300px" height="300px" class="item-img">
-					<a href="#"><button class="btn-item btn">подробнее</button></a>
-					<h3 class="price-item">900р</h3>
-				</div>
-				<div class="card-item">
-					<h2 class="item-name">второй тест</h2>
-					<img src="img/fiveBG.jpg" width="300px" height="300px" class="item-img">
-					<a href="#"><button class="btn-item btn">подробнее</button></a>
-					<h3 class="price-item">900р</h3>
-				</div>
+		<div class="one-block">
+			<div class="block">
+				<header>
+					<div class="content">
+						<h1 class="name">домашний текстиль</h1>
+						<p class="nPhone">+7 (904) 214-82-18</p>
+					</div>
+					<div class="BG">
+						<img src="img/oneBG.jpg" width="160" height="180" class="one-bg">
+						<img src="img/twoBG.jpg" width="140" height="160" class="two-bg">
+						<img src="img/threeBG.jpg" width="90" height="110" class="three-bg">
+						<img src="img/fourBG.jpg" width="90" height="110" class="four-bg">
+						<img src="img/fiveBG.jpg" width="140" height="160" class="five-bg">
+						<img src="img/sixBG.jpg" width="160" height="130" class="six-bg">
+					</div>
+					<div class="BB">
+						<img src="img/oneBB.jpg" width="140" height="160" class="one-bb">
+						<img src="img/twoBB.jpg" width="120" height="140" class="two-bb">
+						<img src="img/threeBB.jpg" width="90" height="90" class="three-bb">
+						<img src="img/fourBB.jpg" width="90" height="110" class="four-bb">
+						<img src="img/fiveBB.jpg" width="150" height="100" class="five-bb">
+						<img src="img/sixBB.jpg" width="140" height="160" class="six-bb">
+					</div>
+				</header>
 			</div>
 		</div>
+		<h1 class="whatSelling white">что мы продаем</h1>
+		<div class="two-block">
+			<div class="block border-wrapper">
+				<div class="wrapper">
+					<div class="cards" overflow-y:scroll>
+						<div class="state">
+							<button class="itemBtn-news btn" onclick="location.href='<?php echo "{$_SERVER['PHP_SELF']}?sort_id=1"; ?>'">новинки</button>
+							<button class="itemBtn-popular btn" onclick="location.href='<?php echo "{$_SERVER['PHP_SELF']}?sort_id=2"; ?>'">популярное</button>
+							<button class="itemBtn-premium btn" onclick="location.href='<?php echo "{$_SERVER['PHP_SELF']}?sort_id=3"; ?>'">по алфавиту</button>
+						</div>
+						<h1 class="error">ошибка, товары не найдены</h1> <!--чтоб высветить убери 84 строку в style.css!-->
+						<div class="card-item">
+							<img src="img/fiveBG.jpg" class="item-img">
+							<div class="margin-item">
+								<a href="#"><button class="btn-item">подробнее</button></a>
+								<h2 class="item-name">тест</h2>
+							</div>
+						</div>	
+					</div>
+				</div>
+				</div>
+		<div class="two-block">
 		<div class="map">
-			<h1 class="maps-name">мы на картах</h1>
+			<div class="map-name-block">
+				<h1 class="map-name">мы на картах</h1>
+			</div>
 			<iframe class="mainMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2470.651178783211!2d39.309211654928504!3d51.73941519012627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x413b244fb6eedd31%3A0xef6eb1901cff5a3d!2z0YPQuy4g0KTRkdC00L7RgNCwINCi0Y7RgtGH0LXQstCwLCA5NdCcLCDQktC-0YDQvtC90LXQtiwg0JLQvtGA0L7QvdC10LbRgdC60LDRjyDQvtCx0LsuLCAzOTQwNTA!5e0!3m2!1sru!2sru!4v1581964101336!5m2!1sru!2sru" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 		</div>
 		<footer>
 			<a href="admin.htm"><div class="adminBox"></div></a>
 			<p class="nPhone footerPhone">контакты<br>+7 (904) 214-82-18</p>
 		</footer>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 		<script src="javascript.js"></script>
 </body>
 </html>
