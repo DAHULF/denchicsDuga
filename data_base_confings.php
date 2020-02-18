751e06a3d0658607c96c5fd4a30be9b529d136bd
DAHULF <61146735+DAHULF@users.noreply.github.com>
2020-02-18 15:14:47 +0300

    Create data_base_confings.php

---
+++ data_base_confings.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('DATA_BASE_HOST', '127.0.0.1');
+    define('DATA_BASE_USER', 'adminsUser');
+    define('DATA_BASE_PASSWORD', 'adminPassword');
+    define('DATA_BASE_NAME', 'workPolygon');
+
+?>
