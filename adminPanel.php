8bcc536bde8b204403191c023b43994033d4cc13
kriper0nind <kriper0nind@gmail.com>
2020-02-23 17:31:24 +0300

    final frontend

--- adminPanel.php
+++ adminPanel.php
@@ -5,10 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style/admin.css">
   <link rel="stylesheet" href="style/item.css">
+  <link rel="stylesheet" href="style/style.css">
   <title>Document</title>
 </head>
 <body>
-  <div class="create-card">
+<div class="create-card">
     <form>
       <textarea type="pattern" placeholder="описание" class="description-item" rows="0" maxlength="600"></textarea>
       <button type="submit" class="btn btn-submit">создать</button>
@@ -22,16 +23,29 @@
       <input type="number" placeholder="цена" class="txt-item txt-price">
     </form>
   </div>
-  <div class="cards" overflow-y:scroll>
-    <div class="block-item">
-      <div class="card-item">
-        <img src="img/fiveBG.jpg" class="item-img">
-        <div class="margin-item">
-          <a href="#"><button class="btn-item">подробнее</button></a>
-          <h2 class="item-name">dasasasdads</h2>
+  <div class="wrapper">
+    <div class="cards" overflow-y:scroll>
+        <div class='card-item'>
+          <div class='margin-item'>
+          <h2 class='item-name'>тест</h2>
+          <p class="item-descriprion">
+          dssdaasd sadklas dld;jad asdjll;dldjldasjljasdl;djasl
+          </p>
+          <h3 class="item-price">400р</h3>
+          <div class="delete-item">удалить</div>
+          <div class="edit-item">редактировать</div>
+          </div>
+          <div class="owl-carousel owl-theme owl-close item-img">
+          <img src="img/oneBB.jpg" class='item-img'>
+          <img src="img/twoBB.jpg" class='item-img'>
+          <img src="img/threeBB.jpg" class='item-img'>
+          <img src="img/fourBB.jpg" class='item-img'>
+          <img src="img/fiveBB.jpg" class='item-img'> 
+          <img src="img/sixBB.jpg" class='item-img'>
         </div>
-        </div>
-    </div>
-  </div>	
+      </div>
+  </div>
+  </div>
+  <script src="javascript.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -58,7 +58,6 @@
     
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
 	<link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="owl.carousel.min.css">
 	<title>Document</title>
